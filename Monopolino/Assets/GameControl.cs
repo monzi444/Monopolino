bc30d8c385c5009f97a26fe4b1cf4f7ec5c87911
monzi444 <simone.monzardo@gmail.com>
2023-02-02 16:12:04 +0100

    added imprevisti
    creating imprevistiCards objects and extracting a random one when the player goes on the cell.
    it only prints the extracted card infos on the debug console

--- Monopolino/Assets/GameControl.cs
+++ Monopolino/Assets/GameControl.cs
@@ -1,20 +1,67 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Linq;
+using Random = System.Random;
+
+
+//class CartaImprevisto containing card datas, the Action delegates contains what to do on card draw
+public class ImprevistoCard
+{
+    public int id;
+    public string name;
+    public string description;
+    public Delegate Action;
+
+    public ImprevistoCard(int id, string name, string description, Delegate action)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.Action = action;
+    }
+}
+
+
+//KeyCaluePair with infos of the cards (serializable in inspector)
+[Serializable]
+public class info
+{
+    public string name;
+    public string description;
+}
 
 public class GameControl : MonoBehaviour {
 
 
     //initialising variables
+
+    #region gameObjects
     private static GameObject player1, player2;
 
-    public static int diceSideThrown = 0;
-
     public static GameObject dice1, dice2;
+    #endregion
 
+
+    #region dice roll and player movement
     //whosTurn: 1->Player1; 2->Player2
     private static int whosTurn = 1;
+    
+    public static int diceSideThrown = 0;
+    #endregion
 
+    #region imprevisti
+    //list of info, contains all the cards info
+    public List<info> cardsInfoImprevisti = new List<info>();
+
+    //list of cards
+    public List<ImprevistoCard> cards = new List<ImprevistoCard>();
+  
+    delegate void Action(GameObject player);
+    #endregion
     // Use this for initialization
     void Start () {
 
@@ -24,8 +71,40 @@ public class GameControl : MonoBehaviour {
 
         dice1 = GameObject.Find("Dice1");
         dice2 = GameObject.Find("Dice2");
+
+
+
+        //on start filling each card with their info
+        //in this method are established delegates actions (what to do on card draw)
+        for (int i = 0; i < cardsInfoImprevisti.Count(); i++)
+        {
+            switch (i)
+            {
+                case 0:
+                    Action action0 = delegate (GameObject player) {
+
+                    };
+                    cards.Add(new ImprevistoCard(i, cardsInfoImprevisti[i].name, cardsInfoImprevisti[i].description, action0));
+                    break;
+                case 1:
+                    Action action1 = delegate (GameObject player) {
+
+                    };
+                    cards.Add(new ImprevistoCard(i, cardsInfoImprevisti[i].name, cardsInfoImprevisti[i].description, action1));
+                    break;
+                case 2:
+                    Action action2 = delegate (GameObject player) {
+
+                    };
+                    cards.Add(new ImprevistoCard(i, cardsInfoImprevisti[i].name, cardsInfoImprevisti[i].description, action2));
+                    break;
+            }
+
+        }
     }
 
+
+    #region dice roll and player movement
     //called on dice click
     public IEnumerator Roll()
     {
@@ -82,4 +161,5 @@ public class GameControl : MonoBehaviour {
                 break;
         }
     }
+    #endregion
 }
---
+++ Monopolino/Assets/Imprevisti.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting;
+using UnityEngine;
+using System.Linq;
+using Random = System.Random;
+
+public class Imprevisti : MonoBehaviour
+{
+    private static GameObject player;
+    
+
+    private static GameObject control;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GameObject.Find("GameControl");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        player = collision.gameObject;
+        DrawCard(player);
+    }
+
+    private void DrawCard(GameObject player)
+    {
+        Random r = new Random();
+        ImprevistoCard card = control.GetComponent<GameControl>().cards[r.Next(0,control.GetComponent<GameControl>().cards.Count())];
+        Debug.Log(card.name + card.description);
+    }
+}
